77ed10e11a98f1c92cb64bfd785ece4634a2775d
eliasgiannantonio <giannantonioelias@gmail.com>
2023-05-31 17:38:07 -0300

    Correcciones en el Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-scroll';
 
 import Logo from "../assets/img/logo.svg";
 import Nav from "./Nav";
 import NavMobile from "./NavMobile";
-import Socials from "./Socials"
+import Socials from "./Socials";
 
 const Header = () => {
-    const [bg, setBg] = useState(false);
+  const [bg, setBg] = useState(false);
 
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            return window.scrollY > 50 ? setBg(true) : setBg(false);
-        })
-    })
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 50 ? setBg(true) : setBg(false);
+    };
 
-    return (
-        <header className={`${bg ? 'bg-tertiary h-20' : 'h-24'} flex items-center fixed top-0 w-full text-white z-10 transition-all duration-300`} >
-            <div className="container mx-auto h-full flex items-center justify-between">
-                <a href="#">
-                    <img src={Logo} alt="" className='w-20' />
-                </a>
-                <div className='hidden lg:block'>
-                    <Nav />
-                </div>
-                <div className='hidden lg:block'>
-                    <Socials />
-                </div>
-                <div className='lg:hidden'>
-                    <NavMobile />
-                </div>
+    window.addEventListener("scroll", handleScroll);
 
-            </div>
-        </header >
-    )
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className={`${bg ? 'bg-tertiary h-20' : 'h-24'} flex items-center fixed top-0 w-full text-white z-10 transition-all duration-300`} >
+      <div className="container mx-auto h-full flex items-center justify-between">
+        <Link to="inicio" smooth={true} duration={500}><button><img src={Logo} alt="" className='w-20' /></button>
+        </Link>
+        <div className='hidden lg:block'>
+          <Nav />
+        </div>
+        <div className='hidden lg:block'>
+          <Socials />
+        </div>
+        <div className='lg:hidden'>
+          <NavMobile />
+        </div>
+      </div>
+    </header >
+  );
 }
 
-export default Header
+export default Header;
